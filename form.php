5ed00c36dd038ab6776dc3ad054ee7adea28cfe9
Axel Auza A <axelauza31@gmail.com>
2024-11-25 23:38:03 -0500

    Add form and php script

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input Form</title>
+</head>
+<body>
+    <h2>Enter a List of Integers</h2>
+    <form action="process.php" method="get">
+        <label for="integers">Enter integers separated by commas:</label>
+        <input type="text" id="integers" name="integers" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
